1f4e16b01e5c9b4daeb5324f82e96f1d5bf967c7
zhoujuliana <56312807+zhoujuliana@users.noreply.github.com>
2020-09-24 17:56:05 -0400

    Update MUSA508_TOD_Code.R
    don't know what I'm doing wrong

--- MUSA508_TOD_Code.R
+++ MUSA508_TOD_Code.R
@@ -13,6 +13,7 @@ library(tidycensus)
 library(sf)
 library(kableExtra)
 library(viridis)
+library(RSocrata)
 
 options(scipen=999)
 options(tigris_class = "sf")
@@ -306,19 +307,153 @@ kable(allTracts.Summary) %>%
 
 # ---- Part 6. Line Plot as Function of Distance ----
 
+QNSboundary <-
+  rbind(
+    st_read("https://data.cityofnewyork.us/resource/7t3b-ywvw.geojson") %>% 
+      select(boro_name)) %>%
+  dplyr::filter(boro_name == "Queens")
+
+square <-  
+  st_sfc(st_polygon(list(cbind(c(0,3,3,0,0),c(0,0,3,3,0))))) %>%
+
+multipleRingBuffer <- function(inputPolygon, maxDistance, interval) 
+{
+  #create a list of distances that we'll iterate through to create each ring
+  distances <- seq(0, maxDistance, interval)
+  #we'll start with the second value in that list - the first is '0'
+  distancesCounter <- 2
+  #total number of rings we're going to create
+  numberOfRings <- floor(maxDistance / interval)
+  #a counter of number of rings
+  numberOfRingsCounter <- 1
+  #initialize an otuput data frame (that is not an sf)
+  allRings <- data.frame()
+  
+  #while number of rings  counteris less than the specified nubmer of rings
+  while (numberOfRingsCounter <= numberOfRings) 
+  {
+    #if we're interested in a negative buffer and this is the first buffer
+    #(ie. not distance = '0' in the distances list)
+    if(distances[distancesCounter] < 0 & distancesCounter == 2)
+    {
+      #buffer the input by the first distance
+      buffer1 <- st_buffer(inputPolygon, distances[distancesCounter])
+      #different that buffer from the input polygon to get the first ring
+      buffer1_ <- st_difference(inputPolygon, buffer1)
+      #cast this sf as a polygon geometry type
+      thisRing <- st_cast(buffer1_, "POLYGON")
+      #take the last column which is 'geometry'
+      thisRing <- as.data.frame(thisRing[,ncol(thisRing)])
+      #add a new field, 'distance' so we know how far the distance is for a give ring
+      thisRing$distance <- distances[distancesCounter]
+    }
+    
+    
+    #otherwise, if this is the second or more ring (and a negative buffer)
+    else if(distances[distancesCounter] < 0 & distancesCounter > 2) 
+    {
+      #buffer by a specific distance
+      buffer1 <- st_buffer(inputPolygon, distances[distancesCounter])
+      #create the next smallest buffer
+      buffer2 <- st_buffer(inputPolygon, distances[distancesCounter-1])
+      #This can then be used to difference out a buffer running from 660 to 1320
+      #This works because differencing 1320ft by 660ft = a buffer between 660 & 1320.
+      #bc the area after 660ft in buffer2 = NA.
+      thisRing <- st_difference(buffer2,buffer1)
+      #cast as apolygon
+      thisRing <- st_cast(thisRing, "POLYGON")
+      #get the last field
+      thisRing <- as.data.frame(thisRing$geometry)
+      #create the distance field
+      thisRing$distance <- distances[distancesCounter]
+    }
+    
+    #Otherwise, if its a positive buffer
+    else 
+    {
+      #Create a positive buffer
+      buffer1 <- st_buffer(inputPolygon, distances[distancesCounter])
+      #create a positive buffer that is one distance smaller. So if its the first buffer
+      #distance, buffer1_ will = 0. 
+      buffer1_ <- st_buffer(inputPolygon, distances[distancesCounter-1])
+      #difference the two buffers
+      thisRing <- st_difference(buffer1,buffer1_)
+      #cast as a polygon
+      thisRing <- st_cast(thisRing, "POLYGON")
+      #geometry column as a data frame
+      thisRing <- as.data.frame(thisRing[,ncol(thisRing)])
+      #add teh distance
+      thisRing$distance <- distances[distancesCounter]
+    }  
+    
+    #rbind this ring to the rest of the rings
+    allRings <- rbind(allRings, thisRing)
+    #iterate the distance counter
+    distancesCounter <- distancesCounter + 1
+    #iterate the number of rings counter
+    numberOfRingsCounter <- numberOfRingsCounter + 1
+  }
+  
+  #convert the allRings data frame to an sf data frame
+  allRings <- st_as_sf(allRings)
+}
+
+allTracts.rings <-
+  st_join(st_centroid(dplyr::select(allTracts, GEOID, year)), 
+          multipleRingBuffer(st_union(QnsMTA), 15840, 1320)) %>%
+  st_drop_geometry() %>%
+  left_join(dplyr::select(allTracts, GEOID, MedRent, year), 
+            by=c("GEOID"="GEOID", "year"="year")) %>%
+  st_sf() %>%
+  mutate(distance = distance / 5280) #convert to miles
+
+ggplot() + 
+  geom_sf(data = allTracts.rings, aes(fill = distance))+
+  geom_sf(data=QnsMTA, 
+          #aes(colour = Line), 
+          show.legend = "point", size= 1)
+
+# --------
+
+allTracts.rings <-
+  st_join(st_centroid(dplyr::select(allTracts, GEOID, year)),
+          multipleRingBuffer(st_union(QnsMTA), 47520, 2640)) %>%
+  st_drop_geometry() %>%
+  left_join(dplyr::select(allTracts, GEOID, MedRent, year),
+            by=c("GEOID"="GEOID", "year"="year")) %>%
+  st_sf() %>%
+  mutate(distance = distance / 5280) #convert to miles
+
+allTracts.rings_summary <-
+  allTracts.rings %>%
+  st_drop_geometry() %>%
+  group_by(distance, year) %>%
+  summarize(Rent = mean(MedRent, na.rm=T)) 
+
+#graph
+ggplot(data=allTracts.rings_summary,
+       aes(x = distance, y = MedRent, colour = year)) +
+  geom_point()
+
 
 # ---- Part 7. Crime Data ----
 
-QNScrimedat <-  
-  rbind(
-    st_read("https://data.cityofnewyork.us/resource/qgea-i56i.geojson") %>%
-      dplyr::select(rpt_dt, boro_nm, ky_cd, ofns_desc, law_cat_cd, x_coord_cd, y_coord_cd, latitude, longitude, vic_sex) %>%
-      dplyr::filter(boro_nm == "QUEENS") %>%
-      st_transform('ESRI:102318') %>%
-      st_sf())
+crime_url = "https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i"
+token = "jegityh6EZNecHwu90EIPMnUe"
 
-QNScrimedat$year <- format(as.Date(QNScrimedat$rpt_dt, format="%Y/%m/%d"),"%Y")
-View(QNScrimedat)
+QNScrimedat <-   
+  read.socrata(
+    "https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i?boro_nm=QUEENS", 
+    app_token = token)
+
+QNScrimedat <- 
+  read.socrata("https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i") %>%
+  filter(boro_name == "Queens") %>%
+  dplyr::select(Y = latitude, X = longitude) %>%
+  na.omit() %>%
+  st_as_sf(coords = c("X", "Y"), crs = 2263, agr = "constant") %>%
+  st_transform('ESRI:102318') %>%
+  distinct()
 
 ggplot(allTracts.group)+
   geom_sf(data = st_union(tracts09))+
